eaf68a2b3d5d4668951aa88965ef12acccfeea8f
TaeJeong-Park-dev <taejung3852@gmail.com>
2024-06-11 09:31:35 +0900

    Update web_test.py

--- web_test.py
+++ web_test.py
@@ -97,13 +97,6 @@ if "messages" not in st.session_state:
 if "store" not in st.session_state:
     st.session_state["store"] = dict()
 
-# menu = st.sidebar.selectbox("메뉴", ["메인 페이지", "주간 메뉴"])
-
-# if menu == "메인 페이지":
-#     pass
-# elif menu == "주간 메뉴":
-#     if st.button("이미지 팝업"):
-#         st.image("./menu.jpg", width=200)
 
 if "show_image" not in st.session_state:
     st.session_state["show_image"] = False
@@ -118,11 +111,11 @@ with st.sidebar:
         if session_id in st.session_state["store"]:
             del st.session_state["store"][session_id]  # 특정 세션 ID에 대한 기록만 삭제
         st.experimental_rerun()
-    if st.button("이미지 토글"):
+    if st.button("주간 메뉴"):
         st.session_state["show_image"] = not st.session_state["show_image"]
 
 if st.session_state["show_image"]:
-    st.image("./menu.jpg", width=1000)
+    st.image("./menu.jpg", width=800)
 
 
 from utils import print_messages, StreamHandler
